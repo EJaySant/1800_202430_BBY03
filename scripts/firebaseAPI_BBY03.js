ba06d5bbca093c98af8fd6eb0c522b8c5f4c5eb5
EJaySant <ervinjs06@gmail.com>
2024-10-20 19:53:34 -0700

    Fixed possible key issue

--- scripts/firebaseAPI_BBY03.js
+++ scripts/firebaseAPI_BBY03.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
+    apiKey: "AIzaSyBXeHIquO2saftrelRf-PfApA4satO78vo",
+    authDomain: "comp1800-202430-bby03.firebaseapp.com",
+    projectId: "comp1800-202430-bby03",
+    storageBucket: "comp1800-202430-bby03.appspot.com",
+    messagingSenderId: "1015235424214",
+    appId: "1:1015235424214:web:ab7f9bc7bf6d1b7869fe77"
 };
 
 //--------------------------------------------
