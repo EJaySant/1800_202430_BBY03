f03d19cd55f27ba03d4c7a0289cd1ff2b4c640d6
EJaySant <ervinjs06@gmail.com>
2024-10-28 12:13:13 -0700

    Deleted nightmode from skeleton.js

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -6,12 +6,10 @@ function loadSkeleton(){
             // Do something for the user here.
             console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer.html'));
-            console.log($('#nightTogglePlaceholder').load('./text/night_toggle.html'));
         } else {
             // No user is signed in.
             console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer.html'));
-            console.log($('#nightTogglePlaceholder').load('./text/night_toggle.html'));
         }
     });
 }
