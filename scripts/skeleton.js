6a055514d49736e0e4a722cdd7ced8ab84827455
EJaySant <ervinjs06@gmail.com>
2024-11-04 11:57:15 -0800

    Adjusted skeleton.js and login pages

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,13 +1,8 @@
 function loadSkeleton(){
     firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {                   
-            $('#navbarPlaceholder').load('./text/nav_after_login.html');
+        if (!user) {                   
             $('#footerPlaceholder').load('./text/footer.html');
-        } else {
-            // No user is signed in.
-            $('#navbarPlaceholder').load('./text/nav_before_login.html');
-            $('#footerPlaceholder').load('./text/footer.html');
-        }
+        } 
     });
 }
 loadSkeleton();
