fcbcd580878b8a1a60dd7e623c511f8beddefc48
Carl Manansala <cmanansala3@my.bcit.ca>
2024-11-04 22:05:19 -0800

    changed main.js and main.html to include the time of the post

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,13 @@ function displayCards (collection) {
                 var tags = doc.data().item;
                 var description = doc.data().description;
                 var title = doc.data().title;
+                var time = doc.data().time;
                 let newcard = boxTemplate.content.cloneNode(true);
 
                 newcard.querySelector('.tagHolder').innerHTML = tags;
                 newcard.querySelector('.titleHolder').innerHTML = title;
                 newcard.querySelector('.descriptionHolder').innerHTML = description;
+                newcard.querySelector('.timeHolder').innerHTML = time;
                 
                 document.getElementById("postsHere").appendChild(newcard);
             });
