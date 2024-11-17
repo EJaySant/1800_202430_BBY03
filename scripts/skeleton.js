c795de1434b593bd90e8f6e364fac343083e6e91
EJaySant <ervinjs06@gmail.com>
2024-11-16 19:49:02 -0800

    Added a back button for pages that aren't the main page. Note that the footer is now hard coded instead of being dynamically added after the page loads

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,27 +1,32 @@
-function loadSkeleton(){
+function checkPage() {
+    let leftButton = document.getElementById("leftNavButton");
+    if (window.location.pathname != '/main.html') {
+        leftButton.style.display = "block";
+        leftButton.querySelector("a").href = document.referrer;
+    } else {
+        leftButton.style.display = "none";
+    }
+}
+
+function loadSkeleton() {
     firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {                   
-            $('#footerPlaceholder').load('./text/footer.html');
+        if (user) {
+            checkPage();
+        } else {
+            window.location.href = "index.html";
         }
     });
 }
 loadSkeleton();
 
-var footerElement = document.getElementById("footerPlaceholder");
-footerElement.addEventListener("click", (event) => {
-    var leftNav = document.getElementById("leftNavButton");
-    var centerNav = document.getElementById("centerNavButton");
-    var rightNav = document.getElementById("rightNavButton");
+function hideFooter() {
+    let footerElement = document.getElementById("footernav");
+    let centerNav = document.getElementById("centerNavButton");
     if (footerElement.style.height === "6vh") {
         footerElement.style.height = "5px";
-        leftNav.style.display = "none";
         centerNav.style.display = "none";
-        rightNav.style.display = "none";
     } else {
         footerElement.style.height = "6vh";
-        leftNav.style.display = "block";
         centerNav.style.display = "block";
-        rightNav.style.display = "block";
     }
-
-});
+}
